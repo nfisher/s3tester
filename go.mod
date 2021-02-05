9d4f814fdf5a65230ee2a8526c9f3e3beaac32b4
Nathan Fisher <nfisher@junctionbox.ca>
2021-02-04 21:31:30 -0500

    Simple printing of ze buckets

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/nfisher/s3tester
+
+go 1.15
+
+require github.com/aws/aws-sdk-go v1.37.5
---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+func main() {
+	sess, err := session.NewSession()
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	svc := s3.New(sess)
+
+	input := &s3.ListBucketsInput{}
+
+	result, err := svc.ListBuckets(input)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			fmt.Println(err.Error())
+		}
+		return
+	}
+
+	fmt.Println(result.Buckets)
+}
